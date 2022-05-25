1057799d59f17c4a004a29583cd3e709bebce274
Daniel Doyle <tr33m4n@googlemail.com>
2022-05-25 20:46:47 +0100

    Use new Rector config classes

--- rector.php
+++ rector.php
@@ -1,21 +1,20 @@
 <?php
+declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\Set\ValueObject\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator) : void {
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::EARLY_RETURN);
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::TYPE_DECLARATION);
-    $containerConfigurator->import(SetList::PHP_74);
+return static function (RectorConfig $rectorConfig) : void {
+    $rectorConfig->import(SetList::DEAD_CODE);
+    $rectorConfig->import(SetList::EARLY_RETURN);
+    $rectorConfig->import(SetList::CODE_QUALITY);
+    $rectorConfig->import(SetList::TYPE_DECLARATION);
+    $rectorConfig->import(SetList::PHP_74);
 
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [__DIR__ . '/src']);
-    $parameters->set(Option::SKIP, [
+    $rectorConfig->paths([__DIR__ . '/src']);
+    $rectorConfig->skip([
         RemoveUselessParamTagRector::class,
         RemoveUselessReturnTagRector::class,
         __DIR__ . '/src/Test/*'
