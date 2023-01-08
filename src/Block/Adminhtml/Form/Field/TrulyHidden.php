243eb78f8a53918ac9735b5ecad07cd514654534
Daniel Doyle <tr33m4n@googlemail.com>
2023-01-08 12:54:13 +0000

    Ensure scope label is not displayed in MAP

--- src/Block/Adminhtml/Form/Field/TrulyHidden.php
+++ src/Block/Adminhtml/Form/Field/TrulyHidden.php
@@ -9,10 +9,12 @@ use Magento\Framework\Data\Form\Element\Hidden;
 class TrulyHidden extends Hidden
 {
     /**
-     * @inheritDoc
+     * When rendering the field, the scope label is still displayed. Set to `null` to visually hide the field entirely
+     *
+     * @return null
      */
-    public function getDefaultHtml(): string
+    public function getScope()
     {
-        return '';
+        return null;
     }
 }
