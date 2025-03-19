bca22ce840495f691cd04d5acf715c2e0e46c837
Daniel Doyle <tr33m4n@googlemail.com>
2025-03-19 18:37:32 +0000

    WIP: Debugging JS load issues

--- src/view/adminhtml/requirejs-config.js
+++ src/view/adminhtml/requirejs-config.js
@@ -1,9 +1,5 @@
 var config = {
     config: {
-        mixins: {
-            'mage/adminhtml/form': {
-                'tr33m4n_OauthGmail/js/mage/adminhtml/form-mixin': true
-            }
-        }
+
     }
 };
